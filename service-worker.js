5cb14978c8af3877e7660834fa50ef7ea8872343
seventh07th <oguz07uzun@gmail.com>
2026-06-28 18:23:29 +0300

    Update service worker from version 8 to 9

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// VERSİYONU GÜNCELLEDİK (v7'den v8'e çıktı)
-const CACHE_NAME = "kortenis-v8"; 
+// VERSİYONU GÜNCELLEDİK (v8'den v9'a çıktı)
+const CACHE_NAME = "kortenis-v9"; 
 const ASSETS_TO_CACHE = [
   "./index.html",
 ];
